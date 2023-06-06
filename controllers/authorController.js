6b7c6aabd198e0ef681f908d461ec872bed546e3
AndresEfQ <andresf2641@gmail.com>
2023-06-05 23:55:16 -0500

    Add genre create form

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -1,4 +1,5 @@
 const Author = require('../models/author');
+const Book = require('../models/book');
 const asyncHandler = require('express-async-handler');
 
 // Display list of all Authors
@@ -9,7 +10,21 @@ exports.author_list = asyncHandler(async(req, res, next) => {
 
 // Display detail page for a sepecific Auhor.
 exports.author_detail = asyncHandler(async(req, res, next) => {
-  res.send(`NOT IMPLEMENTED: Author detail: ${req.params.id}`);
+  const [author, booksByAuthor] = await Promise.all([
+    Author.findById(req.params.id).exec(),
+    Book.find({ author: req.params.id }, 'title summary').exec(),
+  ]);
+
+  if (author === null) {
+    const err = new Error('Author not found');
+    err.status = 404;
+    return next(err);
+  }
+  res.render('author_detail', {
+    title: 'Author Detail',
+    author,
+    booksByAuthor,
+  });
 });
 
 // Display Author create form on GET.
--- controllers/genreController.js
+++ controllers/genreController.js
@@ -1,6 +1,9 @@
 const Genre = require('../models/genre');
 const Book = require('../models/book');
+
 const asyncHandler = require('express-async-handler');
+
+const { body, validationResult } = require('express-validator');
 
 // Display list of all Genre.
 exports.genre_list = asyncHandler(async (req, res, next) => {
@@ -29,14 +32,47 @@ exports.genre_detail = asyncHandler(async (req, res, next) => {
 });
 
 // Display Genre create form on GET.
-exports.genre_create_get = asyncHandler(async (req, res, next) => {
-  res.send('NOT IMPLEMENTED: Genre create GET');
-});
+exports.genre_create_get = (req, res, next) => {
+  res.render('genre_form', {
+    title: 'Create Genre'
+  });
+};
 
 // Handle Genre create on POST.
-exports.genre_create_post = asyncHandler(async (req, res, next) => {
-  res.send('NOT IMPLEMENTED: Genre create POST');
-});
+exports.genre_create_post = [
+  // Validate and sanitize the name field.
+  body('name', 'Genre must contain at least 3 characteres')
+    .trim()
+    .isLength({ min: 3 })
+    .escape(),
+
+  asyncHandler(async(req, res, next) => {
+    const errors = validationResult(req);
+
+    // Create a genre object with escaped and trimmed data
+    const genre = new Genre({ name:req.body.name });
+
+    if (!errors.isEmpty()) {
+      // There are errors. Render the form again with sanitized values and error messages.
+      res.render('genre_form', {
+        title: "Create Genre",
+        errors: errors.array(),
+        genre,
+      });
+      return;
+    } else {
+      // Data form is valid.
+      // Check if Genre with same name already exists.
+      const genreExists = await Genre.findOne({ name: req.body.name }).exec();
+      if (genreExists) {
+        res.redirect(genreExists.url);
+      } else {
+        await genre.save();
+        res.redirect(genre.url);
+      }
+    }
+  }),
+];
 
 // Display Genre delete form on GET.
 exports.genre_delete_get = asyncHandler(async (req, res, next) => {
